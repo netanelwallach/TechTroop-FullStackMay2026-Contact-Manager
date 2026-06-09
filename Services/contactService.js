17bccf2baf96c93aa7d88eb2584a82233eea143d
Netanel Wallach <netanelwallach@gmail.com>
2026-06-09 17:05:04 +0300

    contactService - remove method no error handling

--- Services/contactService.js
+++ Services/contactService.js
@@ -1,17 +1,22 @@
 import fs from "fs";
+import { resourceUsage } from "process";
 
 const CONTACTS_FILE = "contacts.json";
 
-function add(name, phone, email) {
-  let contacts = [];
-  let result = {};
-
+function loadContacts() {
   if (fs.existsSync(CONTACTS_FILE)) {
     const data = fs.readFileSync(CONTACTS_FILE, "utf8");
-    contacts = data.trim() ? JSON.parse(data) : [];
+    return data.trim() ? JSON.parse(data) : [];
   }
+  return []; // Return empty array if file doesn't exist yet
+}
+
+function add(name, phone, email) {
+  let result = {};
+  const contacts = loadContacts();
 
   result.size = contacts.length;
+
   if (contacts.some((c) => c.email.toLowerCase() === email.toLowerCase())) {
     result.error = "Email already exists in contacts";
     return result;
@@ -23,4 +28,26 @@ function add(name, phone, email) {
 
   result.error = null;
   return result;
+}
+
+function reomve(email) {
+  const result = {};
+  const contacts = loadContacts();
+
+  result.size = contacts.length;
+
+  const toRemove = contacts.findIndex(
+    (c) => c.email.toLowerCase() === email.toLowerCase(),
+  );
+
+  if (toRemove === -1) {
+    result.deletedName = undefined;
+    return result;
+  } else {
+    result.deletedName = contacts[toRemove].name;
+    contacts.splice(toRemove, 1);
+    fs.writeFileSync(CONTACTS_FILE, JSON.stringify(contacts, null, 2), "utf8");
+
+    return result;
+  }
 }
